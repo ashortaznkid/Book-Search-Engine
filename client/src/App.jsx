7a9a2a1cb65cddb28199b3bf7fa1803e672d5c11
ashortaznkid <mccue.nguyen@gmail.com>
2023-10-04 13:35:24 -0700

    Started app.js changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Outlet />
+      </Router>
+    </ApolloProvider>
   );
 }
 
